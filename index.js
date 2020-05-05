82902d6d2703df56f9c765020d9c1d65839df4d3
Florian Pichler <florian.pichler@simplabs.com>
2020-05-05 17:47:38 +0200

    Add option to exclude CSS

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const mergeTrees = require('broccoli-merge-trees');
 const broccoliPostCSS = require('broccoli-postcss');
 const PresetEnv = require('postcss-preset-env');
 
+const pkg = require('./package.json');
+
 module.exports = {
   name: require('./package').name,
 
   treeForAddon() {
     let tree = this._super(...arguments);
+    let app = this._findHost();
+    let options = typeof app.options === 'object' ? app.options : {};
+    let addonConfig = options[pkg.name] || {};
 
     const addonWithoutStyles = funnel(tree, {
       exclude: ['**/*.css'],
     });
+
+    if (addonConfig.excludeCSS) {
+      return tree;
+    }
 
     const addonStyles = funnel(tree, {
       include: ['ember-promise-modals.css'],
